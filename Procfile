e978977fe2f35e2d44a300f6d62e55d557246c52
trimendos <trimendos@gmail.com>
2016-05-21 16:21:44 +0300

    edit project config

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn heroku_test:app
+web: gunicorn runp-heroku:app
+init: python db_create.py
+upgrade: python db_upgrade.py
--- config.py
+++ config.py
@@ -4,6 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'e-library.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'e-library.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False

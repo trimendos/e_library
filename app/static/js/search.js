854993ae651c8ceab2e75e726ae77e1a057bf325
trimendos <trimendos@gmail.com>
2016-05-22 16:52:02 +0300

    js refactoring

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,7 +1,7 @@
 $(function() {
 
     $('#start').click(function() {
-        $('#search').css("border", "");
+        $('#search').css('border', '');
         $('.errors').html('');
         var search = $('#search').val();
         $.ajax({
@@ -9,39 +9,41 @@ $(function() {
             data: $('form').serialize(),
             type: 'POST',
             success: function(response) {
-                arr = jQuery.parseJSON(response);
+                var arr = jQuery.parseJSON(response);
 
                 if('message' in arr){
-                    $('.col-lg-12').html(arr['message']);
+                    $('.book-block').html(arr.message);
                     return;
                 }
                 if('errors' in arr){
-                    var res = "",
-                        tpl = "<li>[text]</li>";
-                    for(var e in arr['errors']){
-                        res += tpl.replace('[text]', arr['errors'][e])
+                    var res = '',
+                        tpl = '<li>[text]</li>';
+                    for(var e in arr.errors){
+                        res += tpl.replace('[text]', arr.errors[e]);
                     }
                     $('.errors').html(res);
-                    $('#search').css("border", "1px solid red");
+                    $('#search').css('border', '1px solid red');
                     return;
                 }
-                var res = "",
-                    tpl_a = "<h4>Author: [name]</h4><p>Books: [books]</p>",
-                    tpl_b = "<h4>Title: [title]</h4><p>Authors: [authors]</p>";
+                var res = '',
+                    tpl_a = '<h4>Author: [name]</h4><p>Books: [books]</p>',
+                    tpl_b = '<h4>Title: [title]</h4><p>Authors: [authors]</p>';
 
-                if(arr['search_type'] == 'book'){
+                if(arr.search_type == 'book'){
                     for(var book in arr.data){
-                        res += tpl_b.replace('[title]', arr.data[book].title).
-                               replace('[authors]', arr.data[book].authors.join(', '))
+                        res += tpl_b
+                                .replace('[title]', arr.data[book].title)
+                                .replace('[authors]', arr.data[book].authors.join(', '));
                     }
-                    $('.col-lg-12').html(res);
+                    $('.book-block').html(res);
 
-                }else if(arr['search_type'] == 'author'){
+                }else if(arr.search_type == 'author'){
                     for(var book in arr.data){
-                        res += tpl_a.replace('[name]', arr.data[book].name).
-                               replace('[books]', arr.data[book].books.join(', '))
+                        res += tpl_a
+                                .replace('[name]', arr.data[book].name)
+                                .replace('[books]', arr.data[book].books.join(', '));
                     }
-                    $('.col-lg-12').html(res);
+                    $('.book-block').html(res);
                 }
             },
             error: function(error) {
@@ -49,8 +51,9 @@ $(function() {
             }
         });
     });
-    $(".search-param").click(function(){
-        var val = $(this).text();
-        $("button[class='btn dropdown-toggle'").text(val);
+
+    $('.search_form').submit(function(e) {
+        e.preventDefault();
+        $('#start').click();
     });
 });
